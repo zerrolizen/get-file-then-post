31c33af2da868dcbed76a04676ca0fa4efda5076
zerrolizen <69670114+zerrolizen@users.noreply.github.com>
2021-04-06 19:03:38 +0200

    error handling 3

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ const FormData = require('form-data');
 
 
 async function getFileThenPost(options){
-
-    if (!options.getUrl) return new Error("The 'getUrl' option must be of type string. Received undefined. For usage view https://www.npmjs.com/package/get-file-then-post.")
-    if (!options.postUrl) return new Error("The 'postUrl' option must be of type string. Received undefined. For usage view https://www.npmjs.com/package/get-file-then-post.")
-
-    await axiosInstance({
-        url: options.getUrl, 
-        method: 'GET',
-        responseType: 'arraybuffer', 
-
-    }).then(async (response) => { 
-        var formData = new FormData()
-        var fileName = options.fileName ? options.fileName : 'file'
-        formData.append('uploadFile', response.data, fileName)
-        
-        await axiosInstance.post(options.postUrl, formData, { headers: formData.getHeaders() })
-            .catch((err) => {console.log(err)})
-
-    }).catch((err) => {console.log(err)})
+    try{
+        if (!options.getUrl) throw("The 'getUrl' option must be of type string. Received undefined. For usage view https://www.npmjs.com/package/get-file-then-post.")
+        if (!options.postUrl) throw("The 'postUrl' option must be of type string. Received undefined. For usage view https://www.npmjs.com/package/get-file-then-post.")
+    
+        await axiosInstance({
+            url: options.getUrl, 
+            method: 'GET',
+            responseType: 'arraybuffer', 
+    
+        }).then(async (response) => { 
+            var formData = new FormData()
+            var fileName = options.fileName ? options.fileName : 'file'
+            formData.append('uploadFile', response.data, fileName)
+            
+            await axiosInstance.post(options.postUrl, formData, { headers: formData.getHeaders() })
+                .catch((err) => {throw(err)})
+    
+        }).catch((err) => {throw(err)})
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
 
