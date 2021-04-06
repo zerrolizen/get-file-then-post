d611274fe74a9221d35ddee7caf1ab221e29ed67
zerrolizen <69670114+zerrolizen@users.noreply.github.com>
2021-04-06 17:51:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Features
+* Redirect file data from one url to another url
+
+# Installation
+
+`npm i get-file-then-post`
+
+# Usage
+
+```
+const getFileThenPost = require('get-file-then-post');
+
+getFileThenPost({
+    getUrl: "npmjs.com/file.exe",
+    postUrl: "npmjs.com/uploadFile",
+    fileName: "file.exe"
+});
+```
+
+# Options
+
+**getUrl** *- url from which the file data gets requested*
+Type: String
+Required: true
+
+**postUrl** *- url to which the requested file data (type: arraybuffer) gets postet*
+Type: String
+Required: true
+
+**fileName** *- naming with which the file gets postet*
+Type: String
+Default: 'file'
---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const axiosInstance = axios.create()
+const FormData = require('form-data');
+
+async function getFileThenPost(options){
+
+    await axiosInstance({
+        url: options.getUrl, 
+        method: 'GET',
+        responseType: 'arraybuffer', 
+
+    }).then(async (response) => { 
+        var formData = new FormData()
+        var fileName = options.fileName ? options.fileName : 'file'
+        formData.append('uploadFile', response.data, fileName)
+        
+        await axiosInstance.post(options.postUrl, formData, { headers: formData.getHeaders() })
+            .catch((err) => {console.log(err)})
+
+    }).catch((err) => {console.log(err)})
+}
+
+module.exports = getFileThenPost
